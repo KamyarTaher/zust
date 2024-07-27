8014fd74481000524ef9b4afa0958958b80bcdd2
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-07-27 16:43:56 +0200

    resolve modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { createStore } from "./core/createStore";
+export { createStore } from "./core";
 export { createPersistConfig } from "./core/persistConfig";
 export type { Store, StoreOptions, Plugin, Middleware } from "./types";
