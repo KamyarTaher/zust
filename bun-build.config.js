8c8ee609cbe532352c0acba11a913b4cbb6fbf29
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-10-02 12:38:45 +0200

    tn3+ +

--- bun-build.config.js
+++ bun-build.config.js
@@ -1,16 +1,22 @@
-export default [
-  {
-    entrypoints: ["./src/index.ts"],
-    outdir: "./dist",
-    external: ["react", "react-dom", "zustand"],
-    format: "esm",
-    outfile: "index.esm.js",
-  },
-  {
-    entrypoints: ["./src/index.ts"],
-    outdir: "./dist",
-    external: ["react", "react-dom", "zustand"],
-    format: "cjs",
-    outfile: "index.cjs.js",
-  },
-];
+export default {
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+  external: ["react", "react-dom", "zustand"],
+  format: "esm",
+  /**
+   * Add plugins to ensure proper handling of external modules
+   */
+  plugins: [
+    {
+      name: "external-modules",
+      setup(build) {
+        const externals = ["react", "react-dom", "zustand"];
+        for (const mod of externals) {
+          build.onResolve({ filter: new RegExp(`^${mod}$`) }, (args) => {
+            return { path: mod, external: true };
+          });
+        }
+      },
+    },
+  ],
+};
