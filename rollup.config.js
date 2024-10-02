a9ed087515804cced60a4caa5b79509a9e78b1ed
KamyarTaher <61198701+KamyarTaher@users.noreply.github.com>
2024-10-02 23:10:28 +0200

    file declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,45 @@
-import typescript from "@rollup/plugin-typescript"; // Use the official plugin
+// rollup.config.js
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      sourcemap: true,
-      exports: "named",
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  external: ["react", "react-dom", "zustand"],
-  plugins: [
-    typescript({
-      tsconfig: "tsconfig.json",
-      declaration: true,
-      declarationDir: "./dist/types",
-    }),
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    json(),
-  ],
-};
+export default [
+  // First build configuration for JavaScript files
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true,
+        exports: "named",
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: ["react", "react-dom", "zustand"],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      json(),
+      typescript({
+        tsconfig: "tsconfig.json",
+        useTsconfigDeclarationDir: true,
+        clean: true,
+      }),
+    ],
+  },
+  // Second build configuration for TypeScript declarations
+  {
+    input: "./src/types.d.ts",
+    output: [{ file: "dist/types/types.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
